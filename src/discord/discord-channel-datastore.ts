ffbf456c6a326fa1e3a6532dfce2ecf7bf8d4a05
slaurent <slaurent22@pm.me>
2021-01-23 09:53:49 -0500

    add generic DiscordChannelDatastore

---
+++ src/discord/discord-channel-datastore.ts
@@ -0,0 +1,99 @@
+import assert from "assert";
+import type { Client as DiscordClient, Collection } from "discord.js";
+import type { DiscordMessageChannel } from "../util/constants";
+import { getLogger } from "../util/logger";
+
+export enum DBTypes {
+    STRING = "STRING",
+    BOOLEAN = "BOOLEAN",
+    INT = "INT",
+    DATE = "DATE",
+}
+
+interface DBSpec {
+    rowSchema: Array<DBTypes>;
+}
+
+interface DiscordChannelDatastoreConfig {
+    channelId: string;
+    discordClient: DiscordClient;
+    dbSpec: DBSpec;
+}
+
+type DBValueType = string | number | boolean | Date;
+
+
+const ROW_REGEX = /```([^]+?)```/g;
+
+function parseMessageContent(dbSpec: DBSpec, content: string) {
+    const { rowSchema, } = dbSpec;
+    const row = [...content.matchAll(ROW_REGEX)].map(([, captured]) => captured);
+    assert(row.length === rowSchema.length,
+        `Invalid number of matches: expected ${rowSchema.length}, found ${row.length}`);
+    return row.map((value, index) => {
+        const dbType = rowSchema[index];
+        switch (dbType) {
+        case DBTypes.BOOLEAN: {
+            if (value === "true") {
+                return true;
+            }
+            if (value === "false") {
+                return false;
+            }
+            throw new Error(`Invalid ${dbType}: ${value}`);
+        }
+        case DBTypes.STRING: {
+            return value;
+        }
+        case DBTypes.INT: {
+            const num = parseInt(value, 10);
+            if (isNaN(num)) {
+                throw new Error(`Invalid ${dbType}: ${value}`);
+            }
+            return num;
+        }
+        case DBTypes.DATE: {
+            return new Date(value);
+        }
+        default: {
+            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+            throw new Error(`Unknown dbType: ${dbType}`);
+        }
+        }
+    });
+}
+
+export class DiscordChannelDataStore {
+    private _discordClient: DiscordClient;
+    private _dbSpec: DBSpec;
+    private _storeChannel: DiscordMessageChannel;
+    private _logger = getLogger({
+        name: "slaurbot-discord-channel-datastore",
+    });
+
+    constructor({
+        channelId,
+        discordClient,
+        dbSpec,
+    }: DiscordChannelDatastoreConfig) {
+        this._discordClient = discordClient;
+        this._dbSpec = dbSpec;
+        const storeChannel =
+            this._discordClient.channels.cache.get(channelId);
+
+        assert(storeChannel && storeChannel.isText(), `Specified channel ${channelId} is invalid`);
+        this._storeChannel = storeChannel;
+    }
+
+    public async readChannel(): Promise<Collection<string, Array<DBValueType>>> {
+        const messages = this._storeChannel.messages;
+        const messageCollection = await messages.fetch({}, false, true);
+        return messageCollection.mapValues(message => {
+            const { content, } = message;
+            this._logger.info(`msg content: ${content}`);
+            const parsedContent = parseMessageContent(this._dbSpec, content);
+            this._logger.info(`parsed: ${parsedContent.join(" ")}`);
+            return parsedContent;
+        });
+    }
+}
--- src/discord/discord-reader.ts
+++ src/discord/discord-reader.ts
@@ -1,8 +1,7 @@
-import assert from "assert";
 import type { Client as DiscordClient } from "discord.js";
-import type { DiscordMessageChannel } from "../util/constants";
 import { DISCORD_CHANNEL_ID } from "../util/constants";
 import { getLogger } from "../util/logger";
+import { DBTypes, DiscordChannelDataStore } from "./discord-channel-datastore";
 
 interface DiscordReaderConfig {
     discordClient: DiscordClient;
@@ -14,63 +13,17 @@ interface Command {
     message: string;
 }
 
-type DBTypes = "string" | "boolean" | "int" | "date";
-
-interface DBSpec {
-    rowSchema: Array<DBTypes>;
-}
-
-const ROW_REGEX = /```([^]+?)```/g;
-
 const COMMAND_DB_SPEC = {
     rowSchema: [
-        "string",  // command name
-        "boolean", // enablement
-        "string"   // message,
-    ] as Array<DBTypes>,
+        DBTypes.STRING,  // command name
+        DBTypes.BOOLEAN, // enablement
+        DBTypes.STRING   // message,
+    ],
 };
-
-function parseMessageContent(dbSpec: DBSpec, content: string) {
-    const { rowSchema, } = dbSpec;
-    const row = [...content.matchAll(ROW_REGEX)].map(([, captured]) => captured);
-    assert(row.length === rowSchema.length,
-        `Invalid number of matches: expected ${rowSchema.length}, found ${row.length}`);
-    return row.map((value, index) => {
-        const dbType = rowSchema[index];
-        switch (dbType) {
-        case "boolean": {
-            if (value === "true") {
-                return true;
-            }
-            if (value === "false") {
-                return false;
-            }
-            throw new Error(`Invalid ${dbType}: ${value}`);
-        }
-        case "string": {
-            return value;
-        }
-        case "int": {
-            const num = parseInt(value, 10);
-            if (isNaN(num)) {
-                throw new Error(`Invalid ${dbType}: ${value}`);
-            }
-            return num;
-        }
-        case "date": {
-            return new Date(value);
-        }
-        default: {
-            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-            throw new Error(`Unknown dbType: ${dbType}`);
-        }
-        }
-    });
-}
 
 export class DiscordReader {
     private _discordClient: DiscordClient;
-    private _twitchCommandsStoreChannel: DiscordMessageChannel;
+    private _twitchCommandsDataStore: DiscordChannelDataStore;
     private _logger = getLogger({
         name: "slaurbot-discord-reader",
     });
@@ -79,24 +32,21 @@ export class DiscordReader {
         discordClient,
     }: DiscordReaderConfig) {
         this._discordClient = discordClient;
-        const twitchCommandsStoreChannel =
-            this._discordClient.channels.cache.get(DISCORD_CHANNEL_ID.TWITCH_COMMANDS_STORE);
-
-        assert(twitchCommandsStoreChannel && twitchCommandsStoreChannel.isText());
-        this._twitchCommandsStoreChannel = twitchCommandsStoreChannel;
+        this._twitchCommandsDataStore = new DiscordChannelDataStore({
+            channelId: DISCORD_CHANNEL_ID.TWITCH_COMMANDS_STORE,
+            discordClient: this._discordClient,
+            dbSpec: COMMAND_DB_SPEC,
+        });
     }
 
     public async readTwitchCommands(): Promise<Array<Command>> {
-        const messages = this._twitchCommandsStoreChannel.messages;
-        const messageCollection = await messages.fetch({}, false, true);
-        return messageCollection.array().map(({ content, }) => {
-            this._logger.info(`msg content: ${content}`);
-            const parsedContent = parseMessageContent(COMMAND_DB_SPEC, content) as [string, boolean, string];
-            this._logger.info(`parsed: ${parsedContent.join(" ")}`);
+        const channelStoreData = await this._twitchCommandsDataStore.readChannel();
+        return channelStoreData.map(parsedContent => {
+            const command = parsedContent[0] as string;
+            const enabled = parsedContent[1] as boolean;
+            const message = parsedContent[2] as string;
             return {
-                command: parsedContent[0],
-                enabled: parsedContent[1],
-                message: parsedContent[2],
+                command, enabled, message,
             };
         });
     }
