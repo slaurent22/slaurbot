91ac384279430ca770b59e41f7dd327689bae082
slaurent <slaurent22@pm.me>
2022-01-05 20:03:26 -0500

    do less discord stuff

--- src/discord/discord-bot.ts
+++ src/discord/discord-bot.ts
@@ -3,13 +3,9 @@ import type { Client as DiscordClient } from "discord.js";
 import { getEnv } from "../util/env";
 import { getLogger } from "../util/logger";
 import {
-    DISCORD_AUTODELETE_CHANNEL_CONFIG,
-    DISCORD_AUTODELETE_CHECK_INTERVAL,
     DISCORD_CLIENT_INTENTS
 } from "../util/constants";
 import { DiscordNotifier } from "./discord-notifier";
-import { DiscordEventManager } from "./discord-event-manager";
-import { DicordChannelAutodeleter } from "./discord-channel-autodeleter";
 
 const logger = getLogger({
     name: "slaurbot-discord-bot",
@@ -30,38 +26,7 @@ export async function createDiscordClientImp(resolve: ((dc: DiscordClient) => vo
             content: "Hello slaurent I am the Discord Notifier",
         });
 
-        const eventManager = new DiscordEventManager({
-            discordClient: client,
-            discordNotifier: notifier,
-        });
-
-        await eventManager.listen();
-
-        const autodeleteConfig = {
-            channelConfig: DISCORD_AUTODELETE_CHANNEL_CONFIG,
-            checkInterval: DISCORD_AUTODELETE_CHECK_INTERVAL,
-            discordClient: client,
-            discordNotifier: notifier,
-        };
-
-        const autodeleter = new DicordChannelAutodeleter(autodeleteConfig);
-        autodeleter.init();
-
         resolve(client);
-    });
-
-    client.on("messageCreate", message => {
-        // eslint-disable-next-line max-len
-        logger.info(`[DISCORD MSG] USER:'${message.author.id}' CHANNEL:'${message.channel.id}' CONTENT:'${message.content}'`);
-        if (message.content === "!ping") {
-            void message.channel.send("pong!");
-        }
-    });
-
-    client.on("messageUpdate", (oldMessage, newMessage) => {
-        const userId = newMessage.author ? newMessage.author.id : "unknown";
-        // eslint-disable-next-line max-len
-        logger.info(`[DISCORD MSG UPDATE] USER:'${userId}' CHANNEL:'${newMessage.channel.id}' CONTENT:'${String(newMessage.content)}'`);
     });
 
     const {
